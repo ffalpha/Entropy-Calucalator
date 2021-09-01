fd90d092b9b72743c5afc13665f7e86c3a8d8ddb
ffalpha <kalanam217@gmail.com>
2021-09-01 08:31:40 +0530

    Update Readme

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup,find_packages
 
 setup(
     name='entropyshannon',
-    version='0.0.1',
+    version='0.0.2',
     description='A python package for various  type of entropy calculations(Specially Shannon)',
     long_description=open('README.md',encoding='UTF-8').read(),
     long_description_content_type='text/markdown',
